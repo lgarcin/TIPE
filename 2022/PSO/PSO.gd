e33a7b7d380da0e4475511c6d1e997e462187f25
lgarcin <laurent.garcin@gmail.com>
2021-12-29 22:25:02 +0100

    Obstacle PNG

--- 2022/PSO/PSO.gd
+++ 2022/PSO/PSO.gd
@@ -6,22 +6,23 @@ var rng = RandomNumberGenerator.new()
 var gbest: Vector2
 var gbest_obj: float = INF
 var c1 = .1
-var c2 = .2
+var c2 = .1
 var w = .8
 
 func _ready():
 	rng.randomize()
-	for i in 50:
-		var new_particle = particle.instance()
-		var pos = Vector2(rng.randf_range(0.0, 1000.0), rng.randf_range(0.0, 600.0))
-		var obj = objective_function(pos)
-		new_particle.global_position = pos
-		new_particle.pbest = pos
-		new_particle.pbest_obj = obj
-		if obj < gbest_obj:
-			gbest = pos
-			gbest_obj = obj
-		add_child(new_particle)
+	for i in 10:
+		for j in 6:
+			var new_particle = particle.instance()
+			var pos = Vector2((i+1)*100,j*100)
+			var obj = objective_function(pos)
+			new_particle.global_position = pos
+			new_particle.pbest = pos
+			new_particle.pbest_obj = obj
+			if obj < gbest_obj:
+				gbest = pos
+				gbest_obj = obj
+			add_child(new_particle)
 
 func _physics_process(delta):
 	for particle in get_children():
@@ -33,11 +34,18 @@ func _physics_process(delta):
 			if obj<gbest_obj:
 				gbest=particle.pbest
 				gbest_obj=obj
-			particle.velocity=w*particle.velocity+c1*rng.randf()*(particle.pbest-particle.global_position)+c2*rng.randf()*(gbest-particle.global_position)
+			#particle.velocity=w*particle.velocity+c1*rng.randf()*(particle.pbest-particle.global_position)+c2*rng.randf()*(gbest-particle.global_position)
+			particle.velocity=w*particle.velocity+c1*rng.randf()*derivative(particle.global_position)+c2*rng.randf()*(gbest-particle.global_position)
 
 
 #func _process(delta):
 #	pass
 
 func objective_function(v: Vector2):
-	return abs(v.x)+abs(v.y-300)
+	return v.distance_to(Vector2(0,300))
+	#return abs(v.x)+abs(v.y-300)
+
+func derivative(v: Vector2):
+	var dx=objective_function(v+Vector2(1,0))-objective_function(v+Vector2(-1,0))
+	var dy=objective_function(v+Vector2(0,1))-objective_function(v+Vector2(0,-1))
+	return -100*Vector2(dx,dy)
