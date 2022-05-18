3467894fa2ac8d157dd57c9e4bb68be87b9643e1
lgarcin <laurent.garcin@gmail.com>
2022-05-18 21:01:50 +0200

    sir

---
+++ 2022/sir2D.py
@@ -0,0 +1,48 @@
+from pde import FieldCollection, PDEBase, PlotTracker, ScalarField, UnitGrid
+
+
+class SIRPDE(PDEBase):
+    """SIR-model with diffusive mobility"""
+
+    def __init__(
+            self, beta=0.3, gamma=0.9, diffusivity=0.1, bc="auto_periodic_neumann"
+    ):
+        self.beta = beta  # transmission rate
+        self.gamma = gamma  # recovery rate
+        self.diffusivity = diffusivity  # spatial mobility
+        self.bc = bc  # boundary condition
+
+    def get_state(self, s, i):
+        """generate a suitable initial state"""
+        norm = (s + i).data.max()  # maximal density
+        if norm > 1:
+            s /= norm
+            i /= norm
+        s.label = "Susceptible"
+        i.label = "Infected"
+
+        # create recovered field
+        r = ScalarField(s.grid, data=1 - s - i, label="Recovered")
+        return FieldCollection([s, i, r])
+
+    def evolution_rate(self, state, t=0):
+        s, i, r = state
+        diff = self.diffusivity
+        ds_dt = diff * s.laplace(self.bc) - self.beta * i * s
+        di_dt = diff * i.laplace(self.bc) + self.beta * i * s - self.gamma * i
+        dr_dt = diff * r.laplace(self.bc) + self.gamma * i
+        return FieldCollection([ds_dt, di_dt, dr_dt])
+
+
+eq = SIRPDE(beta=2, gamma=0.1)
+
+# initialize state
+grid = UnitGrid([32, 32])
+s = ScalarField(grid, 1)
+i = ScalarField(grid, 0)
+i.data[0, 0] = 1
+state = eq.get_state(s, i)
+
+# simulate the pde
+tracker = PlotTracker(interval=10, plot_args={"vmin": 0, "vmax": 1}, movie="sir2D.gif")
+sol = eq.solve(state, t_range=50, dt=1e-2, tracker=["progress", tracker])
