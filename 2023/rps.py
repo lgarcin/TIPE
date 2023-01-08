2946b45724eab25c3dbb4e9f34a1c44b45ec232b
lgarcin <laurent.garcin@gmail.com>
2023-01-08 08:08:49 +0100

    Update rps.py

--- 2023/rps.py
+++ 2023/rps.py
@@ -1,8 +1,6 @@
 import random
 from collections import defaultdict
 
-ORDER = 6
-
 
 class RPSEngine(object):
     RPS = {
@@ -11,18 +9,19 @@ class RPSEngine(object):
         "S": "R",
     }
 
-    def __init__(self):
+    def __init__(self, order=6):
+        self.order = order
         self.db = defaultdict(lambda: {"R": 0, "P": 0, "S": 0})
         self.state = []
 
     def learn(self, player_input):
-        if len(self.state) == ORDER:
+        if len(self.state) == self.order:
             self.db[tuple(self.state)][player_input] += 1
             del self.state[0]
         self.state.append(player_input)
 
     def decide(self):
-        if len(self.state) == ORDER:
+        if len(self.state) == self.order:
             possible = sorted(self.db[tuple(self.state)].items(), key=lambda i: i[1], reverse=True)
             if possible[0][1] == 0:
                 return random.choice(list(self.RPS.keys()))
@@ -36,8 +35,8 @@ class Player(object):
     @staticmethod
     def play():
         while True:
-            player_input = input("Votre choix : ")
-            if player_input in ("R", "P", "S"):
+            player_input = input("Votre choix : R(ock), P(aper), S(scissors) ou Q(uitter) ")
+            if player_input in ("R", "P", "S", "Q"):
                 break
         return player_input
 
@@ -49,6 +48,8 @@ player_score = 0
 engine_score = 0
 while True:
     inp = player.play()
+    if inp == "Q":
+        break
     out = rps_engine.decide()
     print(out)
     rps_engine.learn(inp)
